4e141bff48dfaeb869ab0af207321647655d1845
Adam Buckley <atbuck1993@icloud.com>
2026-02-28 16:50:44 -0500

    more changes

--- teaching.js
+++ teaching.js
@@ -13,6 +13,7 @@ async function loadProducts() {
         };
         products = await reponse.json();
         renderFeaturedProducts();
+        renderProducts();
 
     } catch(error) {
         console.error('Error Loading products: ', error)
@@ -23,5 +24,32 @@ function renderFeaturedProducts() {
     const container = document.getElementById('featuredProducts');
     const featured = products.slice(0,2);
 
-    container.innerHTML = featured.map((product, index) => );
+    container.innerHTML = featured
+    .map((product, index) => `
+        <div class="featured-product">
+        <div class="product-badge" ${index === 1 ? "dark" : ""}>something here</div>
+
+        <button class="wishlist-btn" onclick="toggleWishlist(${product.id})">
+            <i class="far fa-heart"></i>
+        </button>
+
+        <img src="${product.image}" alt="${product.title}" class="product-image">
+        <div class="product-info">
+            <h4>${product.title.substring(0, 30)}...</h4>
+            <button class="product-price ${index === 1 ? "dark" : ""}" onclick="addToCart(${product.id})">
+            $${product-price}
+            </button>
+        </div>
+    </div>`).join("");
+};
+
+function renderProducts() {
+    const container = document.getElementById('productsGrid');
+    const filteredProducts = 
+        currentCategory === 'all' 
+        ? products : products.filter((p) => p.category === currentCategory);
+
+        if (filteredProducts.length === 0) {
+            container.innerHTML = ``
+        }
 };
